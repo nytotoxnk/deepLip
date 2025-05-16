32c30f927bb8896145d03a748f890d079ba219f2
Aldo Diku <aldodiku@hotmail.com>
2025-05-16 16:45:28 +0200

    issues with the submodule OpenVoice, untracked for now

--- kushtrim_asr.py
+++ kushtrim_asr.py
@@ -1,7 +1,11 @@
 from gradio_client import Client, handle_file
 
-
 audio_path = 'extracted_audios\\20250403_161711.wav'
+
+# kushtrim asr model api
+# Works great for now, but will need to be tested more thoroughly.
+# Takes a long time to transcribe, but the accuracy is great.
+
 
 client = Client("Kushtrim/whisper-large-v3-turbo-shqip")
 result = client.predict(
@@ -12,17 +16,24 @@ transcription = result['text']
 print(transcription)
 
 # technicaly should save the transcription to a file
+# and then the transcription should be translated to target language.
+
+"""
+# voice cloning model api
+# from testing this does not work, but also even the UI interface does not clone the voice 
+# to the level that I want it to be.
+# Will only be used if there are not better models available.
 
 voice_client = Client("maitrix-org/Voila-demo")
 
 reference_processing_result = voice_client.predict(
     ref_audio=handle_file(audio_path),
-    api_name="/get_ref_embs" # Or potentially "/get_ref_embs_1" - check the app details
+    api_name="/get_ref_embs"
 )
 
 tts_result = voice_client.predict(
-		text=transcription,
-		ref_embs=reference_processing_result,
+		text="There is something I do not want to say to you who is someone not even related to me",
 		api_name="/run_tts"
 )
 print(tts_result)
+"""
